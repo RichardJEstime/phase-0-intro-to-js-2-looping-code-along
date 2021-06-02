3ce6e954435f41f7f1aed6b22b7b822ebcad3167
RichardEstime <richardjestime@yahoo.com>
2021-06-02 10:04:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards( [ 'firstPerson','secondPerson','thirdPerson'], 'surprise') {
+    for(let i = 0; i < writeCards.length; i++) {
+        console.log [`Thank you ${writeCards[i]} for the wonderful ${writeCards[1]} gift!`]
+        console.log [`Thank you ${} for the wonderful ${} gift!`]
+        console.log [`Thank you ${} for the wonderful ${} gift!`]
+    } 
+  }
+
+function countDown(number) {
+    let number = 10;
+    while (number < 10){
+        console.log (countDown--)
+    }
+}
